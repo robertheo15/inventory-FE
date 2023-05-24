b04a5af8e49d96b05e8e25907e52d855cd86283f
Robert Theo <robertheo.rt@gmail.com>
2023-05-24 22:33:44 +0700

    Add utils

--- src/utils/title.js
+++ src/utils/title.js
@@ -1,6 +1,12 @@
 const title = {
   login: "Login - Kreasitex Jakarta",
-  register: "Registrasi - Kreasitex Jakarta"
+  forgetPassword: "Forget password - Kreasitex Jakarta",
+  resetPassword: "Reset password - Kreasitex Jakarta",
+  register: "Registrasi - Kreasitex Jakarta",
+  brand: "Kreasitex Jakarta",
+  sales: "Penjualan - Kreasitex Jakarta",
+  expenses: "Pengeluaran - Kreasitex Jakarta",
+  profile: "Profil - Kreasitex Jakarta"
 }
 
 export default title;
