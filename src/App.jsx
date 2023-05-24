d174651823b5f1866000ae6154c7447e69721621
Robert Theo <robertheo.rt@gmail.com>
2023-05-24 22:33:57 +0700

    Fix app

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import React, { useState } from 'react'
 import { Route, Routes } from "react-router-dom";
 
-import './App.css'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
 import RegisterPage from './pages/RegisterPage'
 import DashboardPage from './pages/DashboardPage'
-import ForgetPassword from './pages/ForgetPassword';
+import ForgetPasswordPage from './pages/ForgetPasswordPage';
+import ResetPassword from './pages/ResetPassword';
+import ExpensePage from './pages/ExpensePage';
 
-const  App = () => {
-  let auth = true
+import './styles/volt.css';
+import SalesPage from './pages/SalesPage';
+import ProfilePage from './pages/ProfilePage';
+import RoleManagementPage from './pages/RoleManagementPage';
+import OrderSupplierPage from './pages/OrderSupplierPage';
+import CashierPage from './pages/CashierPage';
+
+const  App = () => {  
+  
+  let auth = true;
   if (auth) {
     return (
       <div className="app">    
         <Routes>
           <Route path="/" element={<HomePage/>} />
           <Route path="/dashboard" element={<DashboardPage/>} />
-          <Route path="/register" element={<RegisterPage/>} />
+          <Route path="/register" element={<RegisterPage/>} /> 
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/forget-password" element={<ForgetPasswordPage/>} />
+          <Route path="/reset-password" element={<ResetPassword/>} />
+          <Route path="/expenses" element={<ExpensePage/>} />
+          <Route path="/sales" element={<SalesPage/>} />
+          <Route path="/profile" element={<ProfilePage/>} />
+          <Route path="/management" element={<RoleManagementPage/>} />
+          <Route path="/orders" element={<OrderSupplierPage/>} />
+          <Route path="/cashier" element={<CashierPage/>} />
+          <Route path="/stocks" element={<StockPage/>} />
         </Routes>
       </div>
     )
@@ -25,7 +44,7 @@ const  App = () => {
       <div className="app">
         <Routes>
           <Route path="/login" element={<LoginPage/>} />
-          <Route path="/forgetpassword" element={<ForgetPassword/>} />
+          <Route path="/forgetpassword" element={<ForgetPasswordPage/>} />
         </Routes>
       </div>
     )
