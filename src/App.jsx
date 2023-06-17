08ef97f59f129d3c2b85018b46ddf91e6b3fb425
Robert Theo <robertheo.rt@gmail.com>
2023-06-17 10:46:36 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,21 @@ import ResetPassword from "./pages/ResetPassword";
 import ExpensePage from "./pages/ExpensePage";
 
 import "../src/styles/volt.css";
-import SalesPage from "./pages/SalesPage";
+import SalesPage from "./pages/transaction/SalesPage";
+import OrderSupplierPage from "./pages/transaction/OrderSupplierPage";
 import ProfilePage from "./pages/ProfilePage";
 import RoleManagementPage from "./pages/RoleManagementPage";
-import OrderSupplierPage from "./pages/OrderSupplierPage";
-import CashierPage from "./pages/CashierPage";
+// import CashierPage from "./pages/cashier/CashierPage";
 import StockPage from "./pages/StockPage";
 import CourierPage from "./pages/CourierPage";
-import AddCartPage from "./pages/AddCartPage";
-import ProductsPage from "./pages/ProductsPage";
+import AddCartPage from "./pages/cashier/AddCartPage";
+import ProductsPage from "./pages/product/ProductsPage";
 import InvoicePage from "./pages/InvoicePage";
-import useTransactions from "./hooks/useTransactions";
+import AddCartPageSupplier from "./pages/AddCartPageSupplier";
+import OrderSupplierReportPage from "./pages/OrderSupplierReportPage";
+import SalesReportPage from "./pages/SalesPageReport";
+import CustomerPage from "./pages/customer/CustomerPage";
+import ProductVariantPage from "./pages/product/ProductVariantPage";
 
 const App = () => {
   let auth = true;
@@ -29,22 +33,25 @@ const App = () => {
       <div className="app">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/forget-password" element={<ForgetPasswordPage />} />
           <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/expenses" element={<ExpensePage />} />
+          <Route path="/expenses" element={<OrderSupplierPage />} />
           <Route path="/sales" element={<SalesPage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/management" element={<RoleManagementPage />} />
-          <Route path="/orders" element={<OrderSupplierPage />} />
-          <Route path="/cashier" element={<CashierPage />} />
+          <Route path="/orders" element={<AddCartPageSupplier />} />
           <Route path="/stocks" element={<StockPage />} />
           <Route path="/couriers" element={<CourierPage />} />
-          <Route path="/carts" element={<AddCartPage />} />
+          <Route path="/cashier" element={<AddCartPage />} />
           <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/variants" element={<ProductVariantPage />} />
+          <Route path="/sales/reports" element={<OrderSupplierReportPage />} />
+          <Route path="/orders/reports" element={<SalesReportPage />} />
           <Route path="/invoices" element={<InvoicePage />} />
+          <Route path="/customers" element={<CustomerPage />} />
         </Routes>
       </div>
     );
